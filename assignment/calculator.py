d48f0fae09fefeb4e8537e26575992b7fb8f1161
makauimmaculate <151067818+makauimmaculate@users.noreply.github.com>
2025-03-14 20:59:54 +0300

    Add files via upload

---
+++ assignment/calculator.py
@@ -0,0 +1,27 @@
+def calculator():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Enter the operation (+, -, *, /): ")
+
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '-':
+            result = num1 - num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Error: Division by zero is not allowed.")
+                return
+        else:
+            print("Error: Invalid operation.")
+            return
+
+        print(f"{num1} {operation} {num2} = {result}")
+    except ValueError:
+        print("Error: Invalid input. Please enter numerical values.")
+
+calculator()
